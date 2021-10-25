261664956a0f2ec49714ca19c67d8acfc04ca445
lumilio <logistic-division@lumilium.com>
2021-10-25 12:39:03 +0200

    aggiunta valori casuali alle chiavi di ogni oggetto

--- es 1/main.js
+++ es 1/main.js
@@ -1,28 +1,40 @@
-const teams = [
+const squadre = [
     {
         nome: "squadra1",
-        punti_fatti: 0,
-        falli_subiti: 0
+        punti: 0,
+        falli: 0
     },
 
     {
         nome: "squadra2",
-        punti_fatti: 0,
-        falli_subiti: 0
+        punti: 0,
+        falli: 0
     },
 
     {
         nome: "squadra3",
-        punti_fatti: 0,
-        falli_subiti: 0
+        punti: 0,
+        falli: 0
     },
 
     {
         nome: "squadra4",
-        punti_fatti: 0,
-        falli_subiti: 0
+        punti: 0,
+        falli: 0
     },
-
 ]
 
-console.log(teams);
+
+
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+for (let index = 0; index < squadre.length; index++) {
+    const element = squadre[index];
+    element.punti = randomIntFromInterval(1, 30)
+    element.falli = randomIntFromInterval(1, 30)  
+}
+
+console.log(squadre);
