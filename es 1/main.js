7cd376c02c1ad60a48bebcf222d96dfa6aa367d7
lumilio <logistic-division@lumilium.com>
2021-10-26 19:23:04 +0200

    #

--- es 1/main.js
+++ es 1/main.js
@@ -1,3 +1,34 @@
+
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore utilizzando foreach
+
+
+
+/*
+* Crea un array vuoto e chiedi all’utente un numero
+* da inserire nell’array. Continua a chiedere i numeri
+* all’utente e a inserirli nell’array fino a quando
+* la somma degli elementi è minore di 50.
+*/ 
+
+
+
+
+
+/* Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+ */
+
+
+
+
+
+
+
+
+
+
 const squadre = [
     {
         nome: "squadra1",
